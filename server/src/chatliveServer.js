ff08e5a6d1f33cc0d9aa9c2b66079a6b0a8388cd
Yarce22 <alejomira96@gmail.com>
2025-07-31 20:59:38 -0500

    Actualizacion de la direccion al client

--- server/src/chatliveServer.js
+++ server/src/chatliveServer.js
@@ -4,7 +4,7 @@ import { subscribeToRoomMessages, sendMessage } from './services/messagesService
 export default function realtimeServer(server) {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://sdh-chatlive-dp1o.vercel.app/",
       methods: ["GET", "POST"]
     }
   });
